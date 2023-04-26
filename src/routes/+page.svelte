6d4c5a406019041b7c12ba2261fa01c1eea28e19
Ben Yarbrough <benyarb@gmail.com>
2023-04-26 17:02:58 -0400

    Functional grid of characters from API

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,68 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	import { request, gql } from 'graphql-request';
+
+	const endpoint = 'https://rickandmortyapi.com/graphql';
+
+	/**
+	 * @type {any[]}
+	 */
+	let characters = [];
+
+	const fetchCharacters = async () => {
+		const query = gql`
+			{
+				characters {
+					results {
+						id
+						name
+						image
+					}
+				}
+			}
+		`;
+
+		const data = await request(endpoint, query);
+		characters = data.characters.results;
+	};
+
+	onMount(fetchCharacters);
+</script>
+
+<div class="grid">
+	{#each characters as character}
+		<div class="card">
+			<img src={character.image} alt={character.name} />
+			<h2>{character.name}</h2>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+		gap: 1rem;
+		padding: 1rem;
+	}
+
+	.card {
+		background-color: #fff;
+		border-radius: 8px;
+		box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+		display: flex;
+		flex-direction: column;
+		overflow: hidden;
+	}
+
+	.card img {
+		height: 250px;
+		object-fit: cover;
+		object-position: center;
+	}
+
+	.card h2 {
+		font-size: 1.5rem;
+		margin: 1rem;
+	}
+</style>
